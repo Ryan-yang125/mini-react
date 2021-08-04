b13c5a4c68f372ad57ec89ff1566805c327a28df
Ryan-yang125 <1598094295@qq.com>
2021-08-05 03:52:34 +0800

    update

---
+++ src/observable/index.js
@@ -0,0 +1,42 @@
+const reactionsMap = {};
+let currentlyRenderingComponent;
+
+const handler = {
+  get(target, key) {
+    if (typeof currentlyRenderingComponent === "undefined") {
+      return target[key];
+    }
+    if (!reactionsMap[key]) {
+      reactionsMap[key] = [currentlyRenderingComponent];
+    }
+    const hasComponent = reactionsMap[key].find(
+      comp => comp.ID === currentlyRenderingComponent.ID
+    );
+    if (!hasComponent) {
+      reactionsMap[key].push(currentlyRenderingComponent);
+    }
+    return target[key];
+  },
+  set(target, key, value) {
+    // TODO
+    reactionsMap[key].forEach(component => component.forceUpdate());
+    target[key] = value;
+    return true;
+  }
+}
+export function store(obj) {
+  return new Proxy(obj, handler)
+}
+
+export function view(component) {
+  return class Observer extends MyComponent {
+    ID = `${Math.floor(Math.random() * 10e9)}`;
+
+    render() {
+      currentlyRenderingComponent = this;
+      const renderValue = super.render();
+      currentlyRenderingComponent = undefined;
+      return renderValue;
+    }
+  };
+}
